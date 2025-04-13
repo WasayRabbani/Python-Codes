027847fe3756ac9c022f411335665cc147561b2b
WasayRabbani <wasayrabbani69@gmail.com>
2025-04-13 10:23:43 +0500

    first commit

---
+++ Lecture_3/Lecture3.py
@@ -0,0 +1,44 @@
+# Data Types
+
+# Primitive Data types
+
+# 1. Int
+num=10,20
+print(type(num))
+print(num)
+
+# 4. Float
+num1=10.1
+print(type(num1))
+
+
+# 2. String
+name="MindWired"
+
+# 3. Boolean
+value=True
+
+# Non-Primitive
+
+# 1. List
+l1=[1,2,3,4,5,6,"Name","Class"]
+
+# 2. Dictionary
+d1={"Name":"Mindwired"}
+
+# 3. Tuple
+t1=(1,2,3)
+print(type(t1))
+
+
+
+#Identifiers
+_var=20
+print(_var)
+Num=10
+Num=30
+print(Num)
+
+
+
+
