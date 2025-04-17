7c7c93546eff9e6e44178184a76e0ff406c53df6
WasayRabbani <wasayrabbani69@gmail.com>
2025-04-17 23:08:34 +0500

    Added Lecture_4 input and output scripts

---
+++ Lecture_4/Output.py
@@ -0,0 +1,16 @@
+# Output
+
+print("Hello world!")
+
+num=20
+name='Alex'
+
+print("Name is",name,"Number is",num)
+
+# Formatted Strings
+
+print(f"Name is {name}. Number is {num}")
+
+# Escape Sequence
+
+print("I am learning Python\"This is 4th Video")
---
+++ Lecture_4/input.py
@@ -0,0 +1,21 @@
+# Inputs
+
+# 1st Approach
+# name=input("Enter your name: ")
+
+# # 2nd Approach
+# print("Enter your name: ")
+# name=input()
+
+# print(name)
+
+# TypeCasting
+
+num=input(("Enter number:"))
+print(type(num))
+
+num=int(num)
+
+print(type(num))
+
+
