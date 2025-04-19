cc59aa59d307da29e492fe0bb9efea7e737e5670
WasayRabbani <wasayrabbani69@gmail.com>
2025-04-19 16:54:22 +0500

    Your message here

---
+++ Lecture_6/comments.py
@@ -0,0 +1,42 @@
+# Hello there there is commented code
+
+# Single Line Comment
+
+# Commented Line 1
+
+
+# Multi Line Comment
+
+
+'''
+lin1
+line2
+line3
+line4
+lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4lin1
+line2
+line3
+line4
+'''
+
