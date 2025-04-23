d68c17b7fcd4fb79c524d3e39ac32a758a29c0dc
WasayRabbani <wasayrabbani69@gmail.com>
2025-04-23 12:14:20 +0500

    edit

---
+++ Lecture_7/operators.py
@@ -0,0 +1,60 @@
+#  Operators
+
+
+x=6
+y=8   
+
+# 1. Arithmetic Operators
+# (+,-,*,/,**,%,//)
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x**y)
+# print(x%y)
+# print(x//y)
+
+
+# 2. Assignment Operator
+# (=,+=,-=,*=,/=,%=,**=,//=)
+
+x=6
+y=8
+
+# x=10
+# x+=5 
+
+# print(x)
+
+# 3. Comparison Operator
+# (==,!=,>,<,>=,<=)
+
+# print(x==y) 
+# print(x!=y) 
+# print(x>y) 
+# print(x<y) 
+
+
+# 4. Logical Operator
+# (and,or,not (mostly for boolean) )
+
+# print (not x>10)
+
+# 5. Identity Operator (for variables)
+# (is,is not)
+# x=4
+# y=3
+# print(x is not y)
+
+# 6. Membership Operator
+# (in,not in)
+
+l1=[1,2,3,4,5]
+a=30
+# print(a not in l1)
+
+# Precedence
+
+print( (  (   x+2   ) * 3 ) /10  )  
+ 
