0966b2d874e9a08cfcda069b545b14ae21c83f31
WasayRabbani <wasayrabbani69@gmail.com>
2025-04-23 14:08:57 +0500

    Lecture_8

---
+++ Lecture_8/Exercise1.py
@@ -0,0 +1,58 @@
+''' Q1: Take two numbers from user and perform following operations
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division 
+4. Modulus '''
+
+''' Q2: Swap two numbers with temp variable & without temp'''
+
+''' Q3: Temperature calculator
+Take value in celsius & convert to fahrenheit
+F=(9/5*C)+32 '''
+
+''' Q4: Take name, age, and favorite color from the user and print a sentence like
+Hello Saad! You are 25 years old and your favorite color is blue." '''
+
+
+# Q1:
+
+# num1=int(input('Enter number 1: '))
+# num2=int(input('Enter number 2: '))
+# add=num1+num2
+# subtract=num1-num2
+# product=num1*num2
+# division=num1/num2
+# mod=num1%num2
+
+# print(f"Addition: {add}\nSubtraction: {subtract}\nProduct: {product}\nDivision: {division}\nModulus: {mod}")
+
+#Q2: With Variable
+x=10
+y=20
+# temp=''
+# temp=10
+# x=20
+# y=temp
+# print(f"X is {x}\nY is {y}")
+
+# Without Temp (variable)
+
+# x,y=y,x
+# print(f"X is {x}\nY is {y}")
+
+# Q3: F=(9/5*C)+32
+
+# celsius=float(input('Enter temperature in Celsius: '))
+# fahrenheit=(9/5*celsius)+32
+# print("Temperature in Fahrenheit is :", fahrenheit)
+
+# Q4: 
+
+name=input('Enter your name: ')
+age=int(input('Enter your age: '))
+color=input('Enter favourite color: ')
+
+print(f"Hello! {name}. Your age is {age}. Your favourite Color is {color}")
+
+
