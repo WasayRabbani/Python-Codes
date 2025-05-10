db8eb523570eeeb02e8f151530ba36783f38c3d0
WasayRabbani <wasayrabbani69@gmail.com>
2025-05-10 22:51:33 +0500

    9 & 10e

---
+++ Lecture_9/string-methods.py
@@ -0,0 +1,13 @@
+str='This is lecture  of Mindwired\'s python playlist. Here we will talk about String methods in is Python'
+str1='A     '
+
+print(len(str1))
+str2=str1.strip()
+print(len(str2))
+
+str4="Hello Fr1iends"
+str5=str4.replace('Hello','Hi')
+
+list1=str4.split()
+print(list1)
+print(str5)
