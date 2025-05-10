e4dfac43a850df4dba1da8dd56d91ed96e4448dc
WasayRabbani <wasayrabbani69@gmail.com>
2025-05-10 22:44:46 +0500

    Lec 10 & 11

---
+++ Shorts/Python/s1.py
@@ -0,0 +1,13 @@
+# MindWired
+
+# Formatted String in python
+# (f-String)
+
+name='Harry'
+age=30
+id=2001
+
+print("Name is",name,"Age is",age,'Employee id is',id)
+
+print(f"Name is {name}. Age is {age}. Employee id is {id}")
+
