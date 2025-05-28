04afa4ed33f1ae02276039870ef4c83462f6968c
WasayRabbani <wasayrabbani69@gmail.com>
2025-05-28 13:07:45 +0500

    11

--- Lecture_11/tuple.py
+++ Lecture_11/tuple.py
@@ -2,24 +2,51 @@
 
 
 # 1. Empty Tuple
-
+t1=()
 
 # 2. Tuple Constructor
-
+t2=tuple([1,2,3,'Asad',True])
+print(t2)
 
 # 3. Tuple Indexing
-
+print(t2[1])
 
 # 4. Tuple Slicing
-
+print(t2[0:3])
 
 # 5. Negative Slicing
 
 
+# Negative Step
+print(t2[-1:-5:-1])
+
+# Positive Step
+print(t2[-5:-1:1])
+
 # 6. Deleting a tuple
+del t2
 
 
 # 7. Tuple Unpacking
 
+tuple1=(1,2,3,4,5,6,7)
+*a,b=tuple1
+print(a)
+print(type(a))
+
 
 # 8. Join Tuple
+
+t3=(1,2,3)
+t4=(5,6,7)
+t10=t3+t4
+print(t10)
+
+# 9. Changing items in a tuple
+
+l2=list(t10)
+l2.append('MindWired')
+print(l2)
+t10=tuple(l2)
+print(t10)
+print(type(t10))
---
+++ Lecture_12/Sets.py
@@ -0,0 +1,87 @@
+# Sets
+
+'''Collection of well defined and distinct items.
+
+1. Ordered
+2. Unchangeable
+3. Duplicates not allowed'''
+
+
+# 1. Making Empty Set
+
+
+# 2. Making a Set
+
+
+
+# 3. Accessing Items in a set
+
+
+
+# 4. Set Constructor
+
+
+# 5. Getting length of set
+
+
+# 6. Set Indexing
+
+
+
+# Some useful Functions
+
+''' 
+a. Adding one element to set
+s1.add()
+
+
+
+b. Adding multple items to set
+s1.update([])
+
+
+
+
+c. Removing item from set
+s1.remove() 
+
+
+
+d. Removing first element of set 
+s1.pop()
+
+
+
+e. Clearing Set/Empty it
+s1.clear()
+
+
+
+f. Deleting a set
+del s1
+
+
+
+g. Doing union operation on sets
+
+
+
+
+h. Doing intersection operation on sets
+
+
+
+i. Difference in two sets
+
+
+
+j. Checking subset
+
+
+
+k. CHecking superset
+
+
+
+'''
+
